7c94b1caf4a7c2550395d3e644218cda831d3560
Andrew Wells <andrewwe98@gmail.com>
2025-07-27 16:12:10 -0500

    app fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Video from './Pages/Video/Video';
-import { BrowserRouter } from 'react-router-dom'
+
 
 
   const App = () => {
@@ -11,10 +11,12 @@ import { BrowserRouter } from 'react-router-dom'
   return (
     <div>
       <Navbar/>
+      <HashRouter>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/video/:categoryId/:videoId' element={<Video/>} />
       </Routes>
+      </HashRouter>
       
     </div>
      
