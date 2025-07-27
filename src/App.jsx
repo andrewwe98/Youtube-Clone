5dff526f58423bb9b8f8ce21ecec264d2f4caa3f
Andrew Wells <andrewwe98@gmail.com>
2025-07-27 15:33:41 -0500

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Video from './Pages/Video/Video';
 
 
   const App = () => {
@@ -7,6 +10,10 @@ import Navbar from './Components/Navbar/Navbar';
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/video/:categoryId/:videoId' element={<Video/>} />
+      </Routes>
     </div>
      
   )
