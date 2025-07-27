da393909a66fed9c708f96d63cc177eb87bbf896
Andrew Wells <andrewwe98@gmail.com>
2025-07-27 17:21:26 -0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Youtube-Clone/', // Set the base path for the application
   plugins: [react()],
-  base: "/Youtube-Clone/", // Set the base path for the application
+   // Set the base path for the application
 })
