2e1593c1d29bc0ea933642275bb89915b8c94176
Andrew Wells <andrewwe98@gmail.com>
2025-07-27 16:45:10 -0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Youtube-Clone"><App /></BrowserRouter>
+    <BrowserRouter><App /></BrowserRouter>
 
     {/* --- IGNORE --- */}    \
 
